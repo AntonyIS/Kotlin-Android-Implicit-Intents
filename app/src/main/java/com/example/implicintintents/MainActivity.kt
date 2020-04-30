3559bfd111e8a380e1fe2ad2e63dbd17a1c43f9a
AntonyIS <antonyshikubu@gmail.com>
2020-04-30 09:17:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/implicintintents/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.example.implicintintents
+
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.email
+import org.jetbrains.anko.makeCall
+import org.jetbrains.anko.sendSMS
+import org.jetbrains.anko.share
+
+class MainActivity : AppCompatActivity() {
+    //For capturing image
+    private val PERMISSION_CODE = 1000;
+    private val IMAGE_CAPTURE_CODE = 1001
+    var image_uri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btncall.setOnClickListener{
+            makeCall("0712234556")
+        }
+
+        btnsms.setOnClickListener{
+            sendSMS("0712234556","wodup")
+        }
+
+        btnshare.setOnClickListener{
+            share("Hey there")
+        }
+
+        btnemail.setOnClickListener{
+            email("abc@gmail","Application for a software engineer at Amazon","Hello sir...")
+        }
+
+        btnmpesa.setOnClickListener{
+            val simToolKitLaunchIntent: Intent? = this@MainActivity.getPackageManager().getLaunchIntentForPackage("com.android.stk")
+            simToolKitLaunchIntent?.let { startActivity(it) }
+        }
+
+        btncamera.setOnClickListener{
+            //if system os is Marshmallow or Above, we need to request runtime permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED){
+                    //permission was not enabled
+                    val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    //show popup to request permission
+                    requestPermissions(permission, PERMISSION_CODE)
+                }
+                else{
+                    //permission already granted
+                    openCamera()
+                }
+            }
+            else{
+                //system os is < marshmallow
+                openCamera()
+            }
+
+
+        }
+
+
+    }
+
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        //camera intent
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //called when image was captured from camera intent
+        if (resultCode == Activity.RESULT_OK){
+            //set image captured to image view
+            img_super.setImageURI(image_uri)
+        }
+    }
+
+    //Requesting permision from the user
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        //called when user presses ALLOW or DENY from Permission Request Popup
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    //permission from popup was granted
+                    openCamera()
+                }
+                else{
+                    //permission from popup was denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
